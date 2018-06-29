ab9f2e25f89225fc49a1263a41d00750826fc52e
resteybar <resteybar@csumb.edu>
2018-06-28 21:24:53 -0700

    Update: Grab Values w/ in <object>

--- read_in_xml/readXMLData.cpp
+++ read_in_xml/readXMLData.cpp
@@ -31,6 +31,8 @@ int main() {
 	DOMDocument *domDocParser = NULL;
 	DOMNodeList *list = NULL;
 
+
+
 	try {
 		itsParser = new XercesDOMParser;
 	} catch(const DOMException &err) {
@@ -56,16 +58,92 @@ int main() {
 		list = domDocParser->getElementsByTagName(source);		// Returns list of '<tag>' found
 		cout << "Tags Found: " << list->getLength() << endl;	// How many <tags> found
 
-		// Grab item
-		for(int i = 0; i < list->getLength(); ++i)
-			nodes.push_back(list->item(i));
+		// Bounding Box
+		DOMNodeList *bndbox = NULL;
+		XMLCh *srcBndBox = NULL;
+		XMLCh *srcName = NULL;
 
-		cout << "Nodes Size: " << nodes.size() << endl;
+		srcName = XMLString::transcode("name");
+		srcBndBox = XMLString::transcode("bndbox");
+
+		for(int i = 0; i < list->getLength(); ++i) {
+
+			DOMNode *node = list->item(i); // Gets the ith <object>
+
+			node->normalize();
+
+			DOMNamedNodeMap *map = node->getAttributes();
+
+			DOMNodeList *length = node->getChildNodes();
+
+
+			for(int k = 0; k < length->getLength(); ++k) {
+				DOMNode *childNode = length->item(k);
+
+				if(childNode->getNodeType() == DOMNode::ELEMENT_NODE) {
+					char *nodeName = XMLString::transcode(childNode->getNodeName());
+					char *nodeValue = XMLString::transcode(childNode->getTextContent());
+					cout << "Name of Node " << k + 1 << ") " << nodeName << endl;
+					cout << "Name of Prefix:" << k + 1 << ") " << nodeValue << endl;
+				}
+			}
+
+			cout << "\n\n";
+
+
+//			char *nodeValue = XMLString::transcode(length->item(5)->getNodeName());
+
+//			if() {
+//				cout << "Found POSE\n";
+//
+//
+//			}
+
+//			cout << "Node Tag " << i + 1 << ": "<< nodeValue << endl;
+//
+//			cout << "Node Length " << i + 1 << ": "<< length->getLength() << endl << endl;
+
+//			char* object = XMLString::transcode(map->item(2)->getNodeName());
+
+//			cout << "Length of Map " << i + 1 << ": "<< map->getLength() << endl;
+//			cout << "Item " << i + 1 << ": "<< object << endl;
+
+
+
+
+
+
+//			char* object = XMLString::transcode(node->getNodeName());
+
+//			cout << object << endl;
+
+//			DOMNodeList *objectChilds = node->getChildNodes();
+//
+//			cout << "Children: " << objectChilds->getLength() << endl;
+
+//			DOMNode *child = objectChilds->item(0);
+//
+//			char* childVal = XMLString::transcode(child->getNodeName());
+//
+//			cout << "Child Value: " << childVal << endl;
+
+//			char* object = XMLString::transcode(node->getNodeName());
+
+//			cout << "# Name: " << object << endl;
+//			bndbox = domDocParser->getElementsByTagName(srcBndBox);
+		}
+
+//		cout << *list->item(0)->getTextContent() << endl;
+
+//		// Grab item
+//		for(int i = 0; i < list->getLength(); ++i)
+//			nodes.push_back(list->item(i));
+//
+//		cout << "Nodes Size: " << nodes.size() << endl;
 
 		// Get Values
-		DOMNodeList *list = nodes[0]->getChildNodes();
-		cout << list->getLength() << endl;
-		cout << *list->item(0)->getNodeName() << endl;
+//		XMLCh *source = XMLString::transcode("name"); 		// Tag wanted
+
 
 	} else {
 	  cout << "Error when attempting to parse the XML file : " << inputXML.c_str() << endl;
